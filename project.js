7481b91a14d3447b317cca02da5fed62aa7e01a1
Daniel Zeljko <danielzeljko@users.noreply.github.com>
2022-10-17 10:51:51 -0700

    Refactor code according to code style handout

--- project.js
+++ project.js
@@ -1,24 +1,22 @@
 "use strict";
 
-const results = document.getElementById("results");
-// document.getElementById("image-url").defaultValue = " https://s36537.pcdn.co/wp-content/uploads/2017/08/A-fluffy-cat-looking-funny-surprised-or-concerned.jpg.optimal.jpg ";
-
 const form = document.getElementById("my-form");
 form.addEventListener("submit", submitHandler);
 
+/** Handles the form submission */
 function submitHandler(evt) {
   evt.preventDefault();
   const url = form.querySelector("input[id='image-url']").value;
   const textTop = form.querySelector("input[id='top-text']").value;
   const textBot = form.querySelector("input[id='bot-text']").value;
 
-  if(textTop.trim() === "" && textBot.trim() === "" ){
-    alert("You can't create an empty meme. A meme needs some text.")
+  if (textTop.trim() === "" && textBot.trim() === "") {
+    alert("You can't create an empty meme. A meme needs some text.");
     return;
   }
 
-  if(url.trim() === ""){
-    alert("An image is required to make a meme.")
+  if (url.trim() === "") {
+    alert("An image is required to make a meme.");
     return;
   }
 
@@ -26,29 +24,21 @@ function submitHandler(evt) {
   form.reset();
 }
 
+/** Handles the meme deletion */
 function handleDeleteButton(evt) {
   const cardId = evt.target.dataset.id;
   const card = document.querySelector(`div[data-id="${cardId}"]`);
-  card.remove()
+  card.remove();
 }
 
+/** Appends the generated meme to div #results
+ *
+ * url: image url
+ * text1: text on top of the image
+ * text2: text on bottom of the image
+ */
 function createMeme(url, text1, text2) {
   const cardId = Math.random();
-
-  const card = document.createElement("div");
-  card.classList = "card";
-  card.setAttribute("data-id", cardId);
-
-  const cardBody = document.createElement("div");
-  cardBody.classList = "card-body";
-
-  const cardFooter = document.createElement("div");
-  cardFooter.classList = "card-footer";
-
-  const deleteBtn = document.createElement("button");
-  deleteBtn.textContent = "Delete";
-  deleteBtn.setAttribute("data-id", cardId)
-  deleteBtn.addEventListener("click", handleDeleteButton);
 
   const img = document.createElement("img");
   img.src = url;
@@ -61,13 +51,27 @@ function createMeme(url, text1, text2) {
   textBot.classList = "fixed fixed-bot";
   textBot.textContent = text2;
 
+  const cardBody = document.createElement("div");
+  cardBody.classList = "card-body";
   cardBody.appendChild(img);
   cardBody.appendChild(textTop);
   cardBody.appendChild(textBot);
+
+  const card = document.createElement("div");
+  card.classList = "card";
+  card.setAttribute("data-id", cardId);
   card.appendChild(cardBody);
 
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Delete";
+  deleteBtn.setAttribute("data-id", cardId);
+  deleteBtn.addEventListener("click", handleDeleteButton);
+
+  const cardFooter = document.createElement("div");
+  cardFooter.classList = "card-footer";
   cardFooter.appendChild(deleteBtn);
   card.appendChild(cardFooter);
 
+  const results = document.getElementById("results");
   results.appendChild(card);
 }
